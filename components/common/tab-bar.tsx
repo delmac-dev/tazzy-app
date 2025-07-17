3af584f3fe5aae9dcb13cba32b56005f71b89c61
Delali Tengue <delalitengue.dev@gmail.com>
2025-07-17 07:15:21 +0000

    feature: complete tab ui

--- components/common/tab-bar.tsx
+++ components/common/tab-bar.tsx
@@ -1,10 +1,13 @@
-import { View } from 'react-native';
+import { Pressable, View } from 'react-native';
 import { BottomTabBarProps } from '@react-navigation/bottom-tabs';
-import { BellSimpleIcon, UserIcon, HouseSimpleIcon, TimerIcon } from "phosphor-react-native";
-import TabAddButton from './tab-add-button';
+import { BellSimpleIcon, UserIcon, HouseSimpleIcon, TimerIcon, PlusIcon } from "phosphor-react-native";
 import TabButton from './tab-button';
+import { useState } from 'react';
+import TabAddOptions from './tab-add-options';
 
 export default function TabBar({ state, navigation }:BottomTabBarProps){
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
     <View className='relative flex flex-row justify-between items-center elevation-lg px-5 h-16 bg-neutral-100'>
       <TabButton
@@ -17,7 +20,19 @@ export default function TabBar({ state, navigation }:BottomTabBarProps){
         isFocused={state.index === 3}
         onPress={() => navigation.navigate('schedules')}
       />
-      <TabAddButton />
+      <View className='overflow-hidden rounded-full mx-3'>
+        <Pressable
+          className="h-16 aspect-square bg-sky-600 rounded-full flex items-center justify-center"
+          onPress={() => setModalVisible(true)}
+          android_ripple={{
+            color: "rgba(223, 242, 254, 0.2)",
+            borderless: false,
+            radius: 32,
+          }}
+        >
+          <PlusIcon size={28} color="#fafafa" />
+        </Pressable>
+      </View>
       <TabButton
         icon={BellSimpleIcon}
         isFocused={state.index === 4}
@@ -28,6 +43,7 @@ export default function TabBar({ state, navigation }:BottomTabBarProps){
         isFocused={state.index === 1}
         onPress={() => navigation.navigate('account')}
       />
+      <TabAddOptions visible={modalVisible} closeModal={() => setModalVisible(false)} />
     </View>
   )
 }
--- components/common/tab-button.tsx
+++ components/common/tab-button.tsx
@@ -1,5 +1,5 @@
 import { Icon } from "phosphor-react-native";
-import { Pressable } from "react-native";
+import { Pressable, View } from "react-native";
 
 type TabBarButtonProps = {
   icon: Icon
@@ -9,11 +9,18 @@ type TabBarButtonProps = {
 
 export default function TabButton(props: TabBarButtonProps) {
   return (
-    <Pressable
-      onPress={() => !props.isFocused? props.onPress(): null}
-      className='flex-1 h-full items-center justify-center'
-    >
-      <props.icon size={28} color={props.isFocused ? '#0284c7': '#141414'} weight={props.isFocused? "duotone": "regular"} />
-    </Pressable>
+    <View className="overflow-hidden flex-1 h-full items-center justify-center">
+      <Pressable
+        onPress={() => !props.isFocused? props.onPress(): null}
+        className='w-full h-full items-center justify-center'
+        android_ripple={{
+          color: "rgba(229, 229, 229, 1)",
+          borderless: false,
+          radius: 48,
+        }}
+      >
+        <props.icon size={28} color={props.isFocused ? '#0284c7': '#141414'} weight={props.isFocused? "duotone": "regular"} />
+      </Pressable>
+    </View>
   )
 }
