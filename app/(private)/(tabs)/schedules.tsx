18439007da46ba7601325265a6f9db1e9d425cbc
Delali Tengue <delalitengue.dev@gmail.com>
2025-07-20 18:03:22 +0000

    feature: project routing setup

--- app/(private)/(tabs)/schedules.tsx
+++ app/(private)/(tabs)/schedules.tsx
@@ -1,6 +1,11 @@
 import Dev2 from "~/components/dev2";
 
 export default function Schedules() {
+  // TODO: show all schedules
+  // TODO: no schedules show empty
+  // TODO: delete schedules, archive done schedules
+  // TODO: filter between active, done and archive
+
   return (
     <Dev2
       title='schedules'
