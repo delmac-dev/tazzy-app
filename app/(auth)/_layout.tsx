fe8a9f76f4c31cdd7a801f6885de5e79a9e0a48b
Delali Tengue <delalitengue.dev@gmail.com>
2025-07-29 17:58:19 +0000

    feature: make progress

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -13,8 +13,8 @@ export default function Layout() {
       <Stack.Protected guard={!isOnboarded}>
         <Stack.Screen name="onboarding" />
       </Stack.Protected>
-      <Stack.Screen name="login" options={{animation: "slide_from_right"}} />
-      <Stack.Screen name="sign-up" options={{animation: "ios_from_right"}} />
+      <Stack.Screen name="login" />
+      <Stack.Screen name="sign-up" />
       <Stack.Screen name="forgot-password" />
       <Stack.Screen name="confirm-otp" />
       <Stack.Screen name="new-password" />
